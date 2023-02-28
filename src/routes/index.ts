2519bffb47b402159441083b07c266db04a26cfb
Renan Jefferson Rodrigues Silva <renanjefferson.silva@gmail.com>
2023-02-28 03:45:49 -0300

    feat: Adding message in main route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,10 @@ import paymentsRoutes from './payments.routes';
 
 const routes = Router();
 
+routes.get('/', (request, response) => {
+  return response.json({ message: 'Mercado Pago API' });
+});
+
 routes.use('/payments', paymentsRoutes);
 
 export { routes };
